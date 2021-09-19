b9879fa4d8892c371bb0fda109a243640e668274
KainSt <ksfgnw@gmail.com>
2021-09-19 23:57:13 +0300

    формируется таблица с командами с одинаковым кол-вом очков. вопрос: где взять протоколы игры, чтобы посчитать новое кол-во очков?

--- src/ScoreComparator.java
+++ src/ScoreComparator.java
@@ -6,7 +6,7 @@ public class ScoreComparator implements Comparator<TeamResult> {
        if (o2.gameScore- o1.gameScore == 0) {
            System.out.println("equality of point!! " + o1.teamName + " - " + o2.teamName);
 
-           return (o2.goalScored- o1.goalScored);///нужно вызывать функцию, которая будет
+           return (o2.goalScored - o2.goalMissed) - (o1.goalScored - o1.goalMissed);///нужно вызывать функцию, которая будет
            // возвращать результат
            // -1/0/+1 внутренними расчетами, в зависимости от итогов игры
        }
--- src/TrainingStart.java
+++ src/TrainingStart.java
@@ -2,20 +2,20 @@ public class TrainingStart {
 
     public static void main(String args[]) {
 
-        Season j1 = new Season();
-        System.out.println(" ");
-        for (int tour = 0; tour < j1.seasonSchedule.length; tour++) {
-            for (int k = 0; k < j1.seasonSchedule[tour].length; k++) {
-                j1.refreshSeasonSchedule(j1, j1.seasonSchedule[tour][k]);
-            }
+    Season j1 = new Season();
+    System.out.println(" ");
+    for (int tour = 0; tour < j1.seasonSchedule.length; tour++) {
+        for (int k = 0; k < j1.seasonSchedule[tour].length; k++) {
+            j1.refreshSeasonSchedule(j1, j1.seasonSchedule[tour][k]);
         }
-        System.out.println(" ");
-        j1.showRankTable(j1.seasonRank.homeStat);
-        j1.showRankTable(j1.seasonRank.guestStat);
-        j1.showRankTable(j1.seasonRank.totalStat);
-        j1.sortRankTable(j1.seasonRank.totalStat);
-        j1.showRankTable(j1.seasonRank.totalStat);
-        System.out.println(" ");
+    }
+    System.out.println(" ");
+    j1.showRankTable(j1.seasonRank.homeStat);
+    j1.showRankTable(j1.seasonRank.guestStat);
+    j1.showRankTable(j1.seasonRank.totalStat);
+    j1.sortRankTable(j1.seasonRank);
+    j1.showRankTable(j1.seasonRank.totalStat);
+    System.out.println(" ");
 
     }
 }
