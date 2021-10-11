f8d74800eb946f04bd702b7496d872e801e8cde6
KainSt <ksfgnw@gmail.com>
2021-10-11 23:51:31 +0300

    что-то закомментировал

--- src/UEFARank/ScoreComparator.java
+++ src/UEFARank/ScoreComparator.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 public class ScoreComparator implements Comparator<TeamResult> {
     @Override
     public int compare(TeamResult o1, TeamResult o2) {
-       if (o2.gameScore- o1.gameScore == 0) {
+       /*if (o2.gameScore- o1.gameScore == 0) {
            //System.out.println("equality of point!! " + o1.teamName + " - " + o2.teamName);
 
            return (o2.goalScored - o2.goalMissed) - (o1.goalScored - o1.goalMissed);///нужно вызывать функцию, которая будет
            // возвращать результат
            // -1/0/+1 внутренними расчетами, в зависимости от итогов игры
-       }
+       }*/
         return o2.gameScore- o1.gameScore;
        }
 
