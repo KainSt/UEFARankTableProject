5d2e83eb54242e2a9eb515cc9953a3df722c66cc
KainSt <ksfgnw@gmail.com>
2021-11-30 00:16:43 +0300

    тренировки разные 30-11-2021

--- src/JavaCollectionsFramework2/HWArrayIter.java
+++ src/JavaCollectionsFramework2/HWArrayIter.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class HWArrayIter {
     public static void main (String [] args) {
-        List<String> listOfString = new ArrayList<>();
+        /*List<String> listOfString = new ArrayList<>();
         //Arrays.asList(new String[]{"1","2","3","4","5"});
         listOfString.add("1");
         listOfString.add("2");
@@ -16,24 +16,75 @@ public class HWArrayIter {
 
         printList(listOfString);
 
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i <= 7; i++) {
        listOfString.add(listOfString.get(listOfString.size()-1));
-            printList(listOfString);
+            System.out.println("");
+      // printList(listOfString);
 
-            for (int k = (listOfString.size()-2); k > 0; k--){
+       for (int k = (listOfString.size()-1); k > 0; k--){
                 listOfString.set(k,listOfString.get(k-1));
-                printList(listOfString);
+                //printList(listOfString);
             }
-            listOfString.remove(listOfString.size()-1);
-            printList(listOfString);
+       listOfString.set(0,listOfString.get(listOfString.size()-1));
+       listOfString.remove(listOfString.size()-1);
+      printList(listOfString);
+        }*/
+
+        /*List<String> listOfString = new ArrayList<>();
+        listOfString.add("1");
+        listOfString.add("2");
+        listOfString.add("3");
+        listOfString.add("4");
+        listOfString.add("5");
+
+        printList(listOfString);
+        System.out.println();
+        listOfString.remove(2);
+
+
+       for (int k = (listOfString.size()-1); k >= 0; k--){
+           System.out.print(listOfString.get(k)+" ");
+           }*/
+        List<String> listOfString = new ArrayList<>();
+        listOfString.add("роза");
+        listOfString.add("лоза");
+        listOfString.add("провоз");
+        listOfString.add("поролон");
+        listOfString.add("лень");
+        listOfString.add("лань");
+        listOfString.add("бронь");
+        listOfString.add("схрон");
+
+        printList(listOfString);
+        fix(listOfString);
+        printList(listOfString);
+
         }
 
-    }
+    public static void fix(List<String> list){
+         for (int i = 0; i <list.size(); i++) {
+             if (list.get(i).contains("р") & list.get(i).contains("л")) {
+                 break;
+             }
+
+             if (list.get(i).contains("р")) {
+                     list.remove(i);
+
+                 }
+
+             if (list.get(i).contains("л")) {
+                     list.add(i, list.get(i));
+                     i++;
+                 }
+             }
+         }
+
+
 
     public static void printList(List list){
         System.out.println();
         for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + " ");
+            System.out.println(list.get(i) + " ");
         }
 
     }
