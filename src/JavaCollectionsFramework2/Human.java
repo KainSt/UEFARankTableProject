817279976aa98d7c1b4d063f386800389d52012a
KainSt <ksfgnw@gmail.com>
2021-12-12 23:39:02 +0300

    тренировки разные 12-12-2021 object

---
+++ src/JavaCollectionsFramework2/Human.java
@@ -0,0 +1,62 @@
+package JavaCollectionsFramework2;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Human {
+    String name;
+    String sex;
+    int age;
+    ArrayList<Human> kids;
+
+    Human(String name, String sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        ArrayList<Human> kids = new ArrayList<>();
+    }
+
+    Human(String name) {
+        this.name = name;
+        ArrayList<Human> kids = new ArrayList<>();
+    }
+
+   private String kidsPrint(Human human){
+        String kidsList ="";
+        if (kids.isEmpty()){
+            return "";
+        }
+       for (Human kid : kids) {
+           kidsList = kidsList + " " + kid.name;
+       }
+        return kidsList;
+   }
+
+    public String toString(Human human){
+        return "Имя: " + human.name+ ", пол: " + human.sex + ", возраст: " + human.age+ ", дети: " + human.kidsPrint(human);
+    }
+
+   public static void main(String [] args){
+
+       Human ivan = new Human("ded Ivan", "male", 70);
+       Human masha = new Human("babka Masha", "female", 65);
+       Human fedya = new Human("papa Fedya", "male", 40);
+       Human lesha = new Human("son Lesha");
+       Human misha = new Human("son Misha");
+       Human mashaJr = new Human("daughter Masha ");
+       ivan.kids.add(fedya);
+       masha.kids.add(fedya);
+       fedya.kids.add(lesha);
+       fedya.kids.add(misha);
+       fedya.kids.add(mashaJr);
+
+      /* System.out.println(ivan.toString(ivan));
+       System.out.println(masha.toString(masha));
+       System.out.println(fedya.toString(fedya));*/
+
+    }
+}
+
+
+
