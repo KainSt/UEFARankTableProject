4adf65463ba15ce7a5d621bffa3d153e63b63329
KainSt <ksfgnw@gmail.com>
2022-01-05 22:31:59 +0300

    тренировки разные 04-01-2022 object

--- src/JavaCollectionsFramework2/FibonacciNumbers.java
+++ src/JavaCollectionsFramework2/FibonacciNumbers.java
@@ -3,30 +3,38 @@ package JavaCollectionsFramework2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalTime;
-
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FibonacciNumbers {
 
     public static void main(String [] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        LocalTime start = LocalTime.now();
-        System.out.println(fibonacci(Integer.parseInt(reader.readLine()),start));
+        Map<Integer,BigInteger> fibonacciMap = new TreeMap();
+        String s= reader.readLine();
+        while (!s.equals(" ")){
+        fibonacciMap.put(Integer.parseInt(s),fibonacci(Integer.parseInt(s),fibonacciMap));
+        System.out.println(fibonacciMap.get(Integer.parseInt(s)));
+        s= reader.readLine();
+        }
+
     }
 
-    public static int fibonacci(int n,LocalTime start){
-        LocalTime now = LocalTime.now();;
-        if ((now.getSecond()- start.getSecond())>10){
-            System.out.println("Error");
-            return 0;
+
+    public static BigInteger fibonacci(int n, Map fMap){
+        if (fMap.containsKey(n)){
+            return (BigInteger) fMap.get(n);
         }
         if (n==1) {
-            return 1;
+            fMap.put(n,BigInteger.ZERO);
+            return BigInteger.ZERO;
         }
         if (n==2) {
-           return 1;
+           fMap.put(n,BigInteger.ONE);
+           return BigInteger.ONE;
        } else {
-           return fibonacci(n-2,start) +fibonacci(n-1,start);
+           return fibonacci(n-2,fMap).add(fibonacci(n-1,fMap));
        }
     }
 
