44197660826be2b691e9aa70c774717bc65c68e4
KainSt <ksfgnw@gmail.com>
2022-01-16 23:00:20 +0300

    тренировки разные 16-01-2022

---
+++ src/SkillBox/Main.java
@@ -0,0 +1,56 @@
+package SkillBox;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main
+{
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) throws ParseException {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        Collections.sort(staff, (o1, o2) -> {
+            if (o1.getSalary().equals(o2.getSalary())) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+            return o1.getSalary().compareTo(o2.getSalary());
+        });
+
+        //staff.forEach(System.out::println);
+        Date start = new SimpleDateFormat(dateFormat).parse("01.01.2017");
+        Date finish = new SimpleDateFormat(dateFormat).parse("31.12.2017");
+        staff.stream().filter(e -> e.getWorkStart().after(start) & e.getWorkStart().before(finish))
+                .max(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for(String line : lines)
+            {
+                String[] fragments = line.split("\t");
+                if(fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
